bc6bc4e1a980712b547b07d3765eaee4f46424cb
LaNMaSteR53 <tjt1980@gmail.com>
2014-08-23 00:42:25 -0400

    Added username enumeration to key 19 in dojo-scavenger to give students an idea of what the username should be.

--- scavenger/admin/index.php
+++ scavenger/admin/index.php
@@ -1,26 +1,25 @@
 <?php
-    function displayform() {
+    function displayform($username = "") {
         echo "<html><head><title>Admin page -- Don't log in unless you should!</title><head><body>";
         echo "<h1>Admin Log In</h1><hr><form method='POST' action='index.php?key06=03c6b06952c750899bb03d998e631860'>";
-        echo "Username: <input type='text' name='username'><br>";
+        echo "Username: <input type='text' name='username' value='$username'><br>";
         echo "Password: <input type='password' name='password'><br>";
         echo "<input type='submit'>";
         echo "</form></body></html>";
-        
     }
 
-
-    if (!isset($_POST["username"])) {
-        // display form
-        displayform();
-        
-    } else {
-        // check log in
-        if ($_POST["username"] == "admin" & $_POST["password"] == "abc123") {
-	    echo base64_decode("S2V5IDE5ID0gOWUzY2ZjNDhlY2NmODFhMGQ1NzY2M2UxMjlhZWYzY2I=");
-        } else {
-            displayform();
+    $username = "";
+    if (isset($_POST["username"])) {
+        if ($_POST["username"] == "admin") {
+            // allows for username harvesting
+            $username="admin";
+            if ($_POST["password"] == "abc123") {
+                // password can be cracked with john.txt
+                echo base64_decode("S2V5IDE5ID0gOWUzY2ZjNDhlY2NmODFhMGQ1NzY2M2UxMjlhZWYzY2I=");
+                exit;
+            }
         }
     }
 
+    displayform($username);
 ?>
